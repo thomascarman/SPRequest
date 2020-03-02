a5de14a9aeaf6daef1a88693ca4f73a6c8cd7a29
Thomas Caramn <thomascarman@users.noreply.github.com>
2020-03-02 14:11:07 -0800

    Add Check Default
    Check spPageContextInfo for the site absolute URL

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ import Lists from "./list.js";
 
 export default class SPRequest {
     constructor(defaultUrl) {
-        if (!defaultUrl || defaultUrl === "")
-            throw new Error("SPReqest requires a URL of a sharepoint site.");
+        if (!defaultUrl || defaultUrl === "") {
+            if (_spPageContextInfo) {
+                this.defaultUrl = _spPageContextInfo.siteAbsoluteUrl;
+            } else {
+                throw new Error("Unable to get site URL.");
+            }
+        } else {
+            this.defaultUrl = defaultUrl;
+        }
 
-        this.defaultUrl = defaultUrl;
-        this.lists = new Lists(this.defaultUrl);
+        if (this.defaultUrl) this.lists = new Lists(this.defaultUrl);
     }
 }
+
+export const spRequest = new SPRequest();

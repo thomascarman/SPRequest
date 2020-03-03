e7a7b307a1cf5ac2c937f61f0a003c40936ada34
Thomas Caramn <thomascarman@users.noreply.github.com>
2020-03-03 07:34:52 -0800

    Add GetItemTypeForListName Function
    Function used to create metadata type for SP

--- util/utilities.js
+++ util/utilities.js
@@ -1,3 +1,7 @@
 export const GetServerRelativeUrl = url => {
     return "/" + url.replace(/^(?:\/\/|[^/]+)*\//, "");
 };
+
+export const GetItemTypeForListName = name => {
+    return { type: `SP.Data.${name.charAt(0).toUpperCase() + name.split(" ").join("_x0020_").slice(1)}ListItem` };
+};

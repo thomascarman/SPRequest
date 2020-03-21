39ee43f5f126edaecd8475e8f9ad56f8b9cc3574
Thomas Carman <thomascarman@users.noreply.github.com>
2020-03-21 15:57:58 -0700

    Add string type to Parameters

--- src/scripts/util/utilities.ts
+++ src/scripts/util/utilities.ts
@@ -1,7 +1,13 @@
-export const GetServerRelativeUrl = url => {
+export const GetServerRelativeUrl = (url: string) => {
     return "/" + url.replace(/^(?:\/\/|[^/]+)*\//, "");
 };
 
-export const GetItemTypeForListName = name => {
-    return { type: `SP.Data.${name.charAt(0).toUpperCase() + name.split(" ").join("_x0020_").slice(1)}ListItem` };
+export const GetItemTypeForListName = (name: string) => {
+    return {
+        type: `SP.Data.${name.charAt(0).toUpperCase() +
+            name
+                .split(" ")
+                .join("_x0020_")
+                .slice(1)}ListItem`
+    };
 };
